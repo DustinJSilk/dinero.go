dca69cec28da13b5aac0b10ab2d562bfb1d2a921
Dustin Silk <dustin@staze.com>
2023-05-10 11:11:45 +0100

    chore: add HaveSameCurrency tests

---
+++ dinero/have_same_amount_test.go
@@ -0,0 +1,67 @@
+package dinero_test
+
+import (
+	"testing"
+
+	"dinero.go/currency"
+	"dinero.go/dinero"
+)
+
+func TestHaveSameAmount(t *testing.T) {
+	type test struct {
+		description string
+		a           dinero.Dinero[int]
+		b           dinero.Dinero[int]
+		expect      bool
+	}
+
+	tests := []test{
+		{
+			description: "returns true when amounts and currencies are equal",
+			a:           dinero.NewDinero(500, currency.USD),
+			b:           dinero.NewDinero(500, currency.USD),
+			expect:      true,
+		},
+		{
+			description: "returns true when amounts are equal",
+			a:           dinero.NewDinero(1000, currency.USD),
+			b:           dinero.NewDinero(1000, currency.USD),
+			expect:      true,
+		},
+		{
+			description: "returns false when amounts are not equal",
+			a:           dinero.NewDinero(1000, currency.USD),
+			b:           dinero.NewDinero(2000, currency.USD),
+			expect:      false,
+		},
+		{
+			description: "returns true when amounts are equal once normalized",
+			a:           dinero.NewDinero(1000, currency.USD),
+			b:           dinero.NewDineroWithScale(10000, currency.USD, 3),
+			expect:      true,
+		},
+		{
+			description: "returns false when amounts are not equal once normalized",
+			a:           dinero.NewDinero(10000, currency.USD),
+			b:           dinero.NewDineroWithScale(10000, currency.USD, 3),
+			expect:      false,
+		},
+	}
+
+	for _, tc := range tests {
+		got := dinero.HaveSameAmount(tc.a, tc.b)
+
+		if tc.expect != got {
+			t.Fatalf("%s expected a: %v, got: %v", tc.description, tc.expect, got)
+		}
+	}
+}
+
+func BenchmarkHaveSameAmount(b *testing.B) {
+	da := dinero.NewDinero(100, currency.USD)
+	db := dinero.NewDinero(100, currency.USD)
+
+	for i := 0; i < b.N; i++ {
+		dinero.HaveSameAmount(da, db)
+	}
+}
--- dinero/scaled_amount.go
+++ dinero/scaled_amount.go
@@ -1,6 +1,6 @@
 package dinero
 
-// Used to create decimal values without floats. 1.89 is expressed as { Amount: 189, Scale: 2 }
+// Used to create decimal values without floats. 1.89 is expressed as { Amount: 189, Scale: 2 }.
 type ScaledAmount[T any] struct {
 	Amount T
 	Scale  T
