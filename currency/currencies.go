dea598b54f88fcd43dbac635bcd73fb9e8346037
Dustin Silk <dustin@staze.com>
2023-05-10 17:01:03 +0100

    feat: add all currencies

--- currency/currencies.go
+++ currency/currencies.go
@@ -1,10 +1,499 @@
 package currency
 
-// Euro.
+// AED is the United Arab Emirates dirham.
+var AED = NewCurrency("AED", 10, 2)
+
+// AFN is the Afghan afghani.
+var AFN = NewCurrency("AFN", 10, 2)
+
+// ALL is the Albanian lek.
+var ALL = NewCurrency("ALL", 10, 2)
+
+// AMD is the Armenian dram.
+var AMD = NewCurrency("AMD", 10, 2)
+
+// ANG is the Netherlands Antillean guilder.
+var ANG = NewCurrency("ANG", 10, 2)
+
+// AOA is the Angolan kwanza.
+var AOA = NewCurrency("AOA", 10, 2)
+
+// ARS is the Argentine peso.
+var ARS = NewCurrency("ARS", 10, 2)
+
+// AUD is the Australian dollar.
+var AUD = NewCurrency("AUD", 10, 2)
+
+// AWG is the Aruban florin.
+var AWG = NewCurrency("AWG", 10, 2)
+
+// AZN is the Azerbaijani manat.
+var AZN = NewCurrency("AZN", 10, 2)
+
+// BAM is the Bosnia and Herzegovina convertible mark.
+var BAM = NewCurrency("BAM", 10, 2)
+
+// BBD is the Barbados dollar.
+var BBD = NewCurrency("BBD", 10, 2)
+
+// BDT is the Bangladeshi taka.
+var BDT = NewCurrency("BDT", 10, 2)
+
+// BGN is the Bulgarian lev.
+var BGN = NewCurrency("BGN", 10, 2)
+
+// BHD is the Bahraini dinar.
+var BHD = NewCurrency("BHD", 10, 3)
+
+// BIF is the Burundian franc.
+var BIF = NewCurrency("BIF", 10, 0)
+
+// BMD is the Bermudian dollar.
+var BMD = NewCurrency("BMD", 10, 2)
+
+// BND is the Brunei dollar.
+var BND = NewCurrency("BND", 10, 2)
+
+// BOB is the Bolivian boliviano.
+var BOB = NewCurrency("BOB", 10, 2)
+
+// BOV is the Bolivian Mvdol.
+var BOV = NewCurrency("BOV", 10, 2)
+
+// BRL is the Brazilian real.
+var BRL = NewCurrency("BRL", 10, 2)
+
+// BSD is the Bahamian dollar.
+var BSD = NewCurrency("BSD", 10, 2)
+
+// BTN is the Bhutanese ngultrum.
+var BTN = NewCurrency("BTN", 10, 2)
+
+// BWP is the Botswana pula.
+var BWP = NewCurrency("BWP", 10, 2)
+
+// BYN is the Belarusian ruble.
+var BYN = NewCurrency("BYN", 10, 2)
+
+// BZD is the Belize dollar.
+var BZD = NewCurrency("BZD", 10, 2)
+
+// CAD is the Canadian dollar.
+var CAD = NewCurrency("CAD", 10, 2)
+
+// CDF is the Congolese franc.
+var CDF = NewCurrency("CDF", 10, 2)
+
+// CHE is the WIR Euro.
+var CHE = NewCurrency("CHE", 10, 2)
+
+// CHF is the Swiss franc.
+var CHF = NewCurrency("CHF", 10, 2)
+
+// CHW is the WIR Franc.
+var CHW = NewCurrency("CHW", 10, 2)
+
+// CLF is the Unidad de Fomento.
+var CLF = NewCurrency("CLF", 10, 4)
+
+// CLP is the Chilean peso.
+var CLP = NewCurrency("CLP", 10, 0)
+
+// CNY is the Renminbi (Chinese) yuan.
+var CNY = NewCurrency("CNY", 10, 2)
+
+// COP is the Colombian peso.
+var COP = NewCurrency("COP", 10, 2)
+
+// COU is the Unidad de Valor Real.
+var COU = NewCurrency("COU", 10, 2)
+
+// CRC is the Costa Rican colón.
+var CRC = NewCurrency("CRC", 10, 2)
+
+// CUC is the Cuban convertible peso.
+var CUC = NewCurrency("CUC", 10, 2)
+
+// CUP is the Cuban peso.
+var CUP = NewCurrency("CUP", 10, 2)
+
+// CVE is the Cape Verdean escudo.
+var CVE = NewCurrency("CVE", 10, 2)
+
+// CZK is the Czech koruna.
+var CZK = NewCurrency("CZK", 10, 2)
+
+// DJF is the Djiboutian franc.
+var DJF = NewCurrency("DJF", 10, 0)
+
+// DKK is the Danish krone.
+var DKK = NewCurrency("DKK", 10, 2)
+
+// DOP is the Dominican peso.
+var DOP = NewCurrency("DOP", 10, 2)
+
+// DZD is the Algerian dinar.
+var DZD = NewCurrency("DZD", 10, 2)
+
+// EGP is the Egyptian pound.
+var EGP = NewCurrency("EGP", 10, 2)
+
+// ERN is the Eritrean nakfa.
+var ERN = NewCurrency("ERN", 10, 2)
+
+// ETB is the Ethiopian birr.
+var ETB = NewCurrency("ETB", 10, 2)
+
+// EUR is the Euro.
 var EUR = NewCurrency("EUR", 10, 2)
 
-// Malagasy ariary.
+// FJD is the Fiji dollar.
+var FJD = NewCurrency("FJD", 10, 2)
+
+// FKP is the Falkland Islands pound.
+var FKP = NewCurrency("FKP", 10, 2)
+
+// GBP is the Pound sterling.
+var GBP = NewCurrency("GBP", 10, 2)
+
+// GEL is the Georgian lari.
+var GEL = NewCurrency("GEL", 10, 2)
+
+// GHS is the Ghanaian cedi.
+var GHS = NewCurrency("GHS", 10, 2)
+
+// GIP is the Gibraltar pound.
+var GIP = NewCurrency("GIP", 10, 2)
+
+// GMD is the Gambian dalasi.
+var GMD = NewCurrency("GMD", 10, 2)
+
+// GNF is the Guinean franc.
+var GNF = NewCurrency("GNF", 10, 0)
+
+// GTQ is the Guatemalan quetzal.
+var GTQ = NewCurrency("GTQ", 10, 2)
+
+// GYD is the Guyanese dollar.
+var GYD = NewCurrency("GYD", 10, 2)
+
+// HKD is the Hong Kong dollar.
+var HKD = NewCurrency("HKD", 10, 2)
+
+// HNL is the Honduran lempira.
+var HNL = NewCurrency("HNL", 10, 2)
+
+// HRK is the Croatian kuna.
+var HRK = NewCurrency("HRK", 10, 2)
+
+// HTG is the Haitian gourde.
+var HTG = NewCurrency("HTG", 10, 2)
+
+// HUF is the Hungarian forint.
+var HUF = NewCurrency("HUF", 10, 2)
+
+// IDR is the Indonesian rupiah.
+var IDR = NewCurrency("IDR", 10, 2)
+
+// ILS is the Israeli new shekel.
+var ILS = NewCurrency("ILS", 10, 2)
+
+// INR is the Indian rupee.
+var INR = NewCurrency("INR", 10, 2)
+
+// IQD is the Iraqi dinar.
+var IQD = NewCurrency("IQD", 10, 3)
+
+// IRR is the Iranian rial.
+var IRR = NewCurrency("IRR", 10, 2)
+
+// ISK is the Icelandic króna.
+var ISK = NewCurrency("ISK", 10, 0)
+
+// JMD is the Jamaican dollar.
+var JMD = NewCurrency("JMD", 10, 2)
+
+// JOD is the Jordanian dinar.
+var JOD = NewCurrency("JOD", 10, 3)
+
+// JPY is the Japanese yen.
+var JPY = NewCurrency("JPY", 10, 0)
+
+// KES is the Kenyan shilling.
+var KES = NewCurrency("KES", 10, 2)
+
+// KGS is the Kyrgyzstani som.
+var KGS = NewCurrency("KGS", 10, 2)
+
+// KHR is the Cambodian riel.
+var KHR = NewCurrency("KHR", 10, 2)
+
+// KMF is the Comoro franc.
+var KMF = NewCurrency("KMF", 10, 0)
+
+// KPW is the North Korean won.
+var KPW = NewCurrency("KPW", 10, 2)
+
+// KRW is the South Korean won.
+var KRW = NewCurrency("KRW", 10, 0)
+
+// KWD is the Kuwaiti dinar.
+var KWD = NewCurrency("KWD", 10, 3)
+
+// KYD is the Cayman Islands dollar.
+var KYD = NewCurrency("KYD", 10, 2)
+
+// KZT is the Kazakhstani tenge.
+var KZT = NewCurrency("KZT", 10, 2)
+
+// LAK is the Lao kip.
+var LAK = NewCurrency("LAK", 10, 2)
+
+// LBP is the Lebanese pound.
+var LBP = NewCurrency("LBP", 10, 2)
+
+// LKR is the Sri Lankan rupee.
+var LKR = NewCurrency("LKR", 10, 2)
+
+// LRD is the Liberian dollar.
+var LRD = NewCurrency("LRD", 10, 2)
+
+// LSL is the Lesotho loti.
+var LSL = NewCurrency("LSL", 10, 2)
+
+// LYD is the Libyan dinar.
+var LYD = NewCurrency("LYD", 10, 3)
+
+// MAD is the Moroccan dirham.
+var MAD = NewCurrency("MAD", 10, 2)
+
+// MDL is the Moldovan leu.
+var MDL = NewCurrency("MDL", 10, 2)
+
+// MGA is the Malagasy ariary.
 var MGA = NewCurrency("MGA", 5, 1)
 
-// US Dollar.
+// MKD is the Macedonian denar.
+var MKD = NewCurrency("MKD", 10, 2)
+
+// MMK is the Myanmar kyat.
+var MMK = NewCurrency("MMK", 10, 2)
+
+// MNT is the Mongolian tögrög.
+var MNT = NewCurrency("MNT", 10, 2)
+
+// MOP is the Macanese pataca.
+var MOP = NewCurrency("MOP", 10, 2)
+
+// MRU is the Mauritanian ouguiya.
+var MRU = NewCurrency("MRU", 5, 1)
+
+// MUR is the Mauritian rupee.
+var MUR = NewCurrency("MUR", 10, 2)
+
+// MVR is the Maldivian rufiyaa.
+var MVR = NewCurrency("MVR", 10, 2)
+
+// MWK is the Malawian kwacha.
+var MWK = NewCurrency("MWK", 10, 2)
+
+// MXN is the Mexican peso.
+var MXN = NewCurrency("MXN", 10, 2)
+
+// MXV is the Mexican Unidad de Inversion.
+var MXV = NewCurrency("MXV", 10, 2)
+
+// MYR is the Malaysian ringgit.
+var MYR = NewCurrency("MYR", 10, 2)
+
+// MZN is the Mozambican metical.
+var MZN = NewCurrency("MZN", 10, 2)
+
+// NAD is the Namibian dollar.
+var NAD = NewCurrency("NAD", 10, 2)
+
+// NGN is the Nigerian naira.
+var NGN = NewCurrency("NGN", 10, 2)
+
+// NIO is the Nicaraguan córdoba.
+var NIO = NewCurrency("NIO", 10, 2)
+
+// NOK is the Norwegian krone.
+var NOK = NewCurrency("NOK", 10, 2)
+
+// NPR is the Nepalese rupee.
+var NPR = NewCurrency("NPR", 10, 2)
+
+// NZD is the New Zealand dollar.
+var NZD = NewCurrency("NZD", 10, 2)
+
+// OMR is the Omani rial.
+var OMR = NewCurrency("OMR", 10, 3)
+
+// PAB is the Panamanian balboa.
+var PAB = NewCurrency("PAB", 10, 2)
+
+// PEN is the Peruvian sol.
+var PEN = NewCurrency("PEN", 10, 2)
+
+// PGK is the Papua New Guinean kina.
+var PGK = NewCurrency("PGK", 10, 2)
+
+// PHP is the Philippine peso.
+var PHP = NewCurrency("PHP", 10, 2)
+
+// PKR is the Pakistani rupee.
+var PKR = NewCurrency("PKR", 10, 2)
+
+// PLN is the Polish złoty.
+var PLN = NewCurrency("PLN", 10, 2)
+
+// PYG is the Paraguayan guaraní.
+var PYG = NewCurrency("PYG", 10, 0)
+
+// QAR is the Qatari riyal.
+var QAR = NewCurrency("QAR", 10, 2)
+
+// RON is the Romanian leu.
+var RON = NewCurrency("RON", 10, 2)
+
+// RSD is the Serbian dinar.
+var RSD = NewCurrency("RSD", 10, 2)
+
+// RUB is the Russian ruble.
+var RUB = NewCurrency("RUB", 10, 2)
+
+// RWF is the Rwandan franc.
+var RWF = NewCurrency("RWF", 10, 0)
+
+// SAR is the Saudi riyal.
+var SAR = NewCurrency("SAR", 10, 2)
+
+// SBD is the Solomon Islands dollar.
+var SBD = NewCurrency("SBD", 10, 2)
+
+// SCR is the Seychelles rupee.
+var SCR = NewCurrency("SCR", 10, 2)
+
+// SDG is the Sudanese pound.
+var SDG = NewCurrency("SDG", 10, 2)
+
+// SEK is the Swedish krona.
+var SEK = NewCurrency("SEK", 10, 2)
+
+// SGD is the Singapore dollar.
+var SGD = NewCurrency("SGD", 10, 2)
+
+// SHP is the Saint Helena pound.
+var SHP = NewCurrency("SHP", 10, 2)
+
+// SLL is the Sierra Leonean leone.
+var SLL = NewCurrency("SLL", 10, 2)
+
+// SOS is the Somali shilling.
+var SOS = NewCurrency("SOS", 10, 2)
+
+// SRD is the Surinamese dollar.
+var SRD = NewCurrency("SRD", 10, 2)
+
+// SSP is the South Sudanese pound.
+var SSP = NewCurrency("SSP", 10, 2)
+
+// STN is the São Tomé and Príncipe dobra.
+var STN = NewCurrency("STN", 10, 2)
+
+// SVC is the Salvadoran colón.
+var SVC = NewCurrency("SVC", 10, 2)
+
+// SYP is the Syrian pound.
+var SYP = NewCurrency("SYP", 10, 2)
+
+// SZL is the Swazi lilangeni.
+var SZL = NewCurrency("SZL", 10, 2)
+
+// THB is the Thai baht.
+var THB = NewCurrency("THB", 10, 2)
+
+// TJS is the Tajikistani somoni.
+var TJS = NewCurrency("TJS", 10, 2)
+
+// TMT is the Turkmenistan manat.
+var TMT = NewCurrency("TMT", 10, 2)
+
+// TND is the Tunisian dinar.
+var TND = NewCurrency("TND", 10, 3)
+
+// TOP is the Tongan paʻanga.
+var TOP = NewCurrency("TOP", 10, 2)
+
+// TRY is the Turkish lira.
+var TRY = NewCurrency("TRY", 10, 2)
+
+// TTD is the Trinidad and Tobago dollar.
+var TTD = NewCurrency("TTD", 10, 2)
+
+// TWD is the New Taiwan dollar.
+var TWD = NewCurrency("TWD", 10, 2)
+
+// TZS is the Tanzanian shilling.
+var TZS = NewCurrency("TZS", 10, 2)
+
+// UAH is the Ukrainian hryvnia.
+var UAH = NewCurrency("UAH", 10, 2)
+
+// UGX is the Ugandan shilling.
+var UGX = NewCurrency("UGX", 10, 0)
+
+// USD is the United States dollar.
 var USD = NewCurrency("USD", 10, 2)
+
+// USN is the United States dollar (next day).
+var USN = NewCurrency("USN", 10, 2)
+
+// UYI is the Uruguay Peso en Unidades Indexadas.
+var UYI = NewCurrency("UYI", 10, 0)
+
+// UYU is the Uruguayan peso.
+var UYU = NewCurrency("UYU", 10, 2)
+
+// UYW is the Unidad previsional.
+var UYW = NewCurrency("UYW", 10, 4)
+
+// UZS is the Uzbekistani soʻm.
+var UZS = NewCurrency("UZS", 10, 2)
+
+// VES is the Venezuelan bolívar.
+var VES = NewCurrency("VES", 10, 2)
+
+// VND is the Vietnamese đồng.
+var VND = NewCurrency("VND", 10, 0)
+
+// VUV is the Vanuatu vatu.
+var VUV = NewCurrency("VUV", 10, 0)
+
+// WST is the Samoan tālā.
+var WST = NewCurrency("WST", 10, 2)
+
+// XAF is the Central African CFA franc.
+var XAF = NewCurrency("XAF", 10, 0)
+
+// XCD is the East Caribbean dollar.
+var XCD = NewCurrency("XCD", 10, 2)
+
+// XOF is the West African CFA franc.
+var XOF = NewCurrency("XOF", 10, 0)
+
+// XPF is the CFP franc.
+var XPF = NewCurrency("XPF", 10, 0)
+
+// YER is the Yemeni rial.
+var YER = NewCurrency("YER", 10, 2)
+
+// ZAR is the South African rand.
+var ZAR = NewCurrency("ZAR", 10, 2)
+
+// ZMW is the Zambian kwacha.
+var ZMW = NewCurrency("ZMW", 10, 2)
+
+// ZWL is the Zimbabwean dollar.
+var ZWL = NewCurrency("ZWL", 10, 2)
