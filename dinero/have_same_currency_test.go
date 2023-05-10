619a8bd93b746856bb980fa41595243c815f04ad
Dustin Silk <dustin@staze.com>
2023-05-10 12:55:33 +0100

    chore: add HaveSameCurrency tests

---
+++ dinero/have_same_currency_test.go
@@ -0,0 +1,55 @@
+package dinero_test
+
+import (
+	"testing"
+
+	"dinero.go/currency"
+	"dinero.go/dinero"
+)
+
+func TestHaveSameCurrency(t *testing.T) {
+	type test struct {
+		description string
+		a           dinero.Dinero[int]
+		b           dinero.Dinero[int]
+		expect      bool
+	}
+
+	tests := []test{
+		{
+			description: "returns true when currencies are equal",
+			a:           dinero.NewDinero(2000, currency.USD),
+			b:           dinero.NewDinero(1000, currency.USD),
+			expect:      true,
+		},
+		{
+			description: "returns false when currencies are not equal",
+			a:           dinero.NewDinero(500, currency.USD),
+			b:           dinero.NewDinero(500, currency.EUR),
+			expect:      false,
+		},
+		{
+			description: "returns true when currencies are structurally equal",
+			a:           dinero.NewDinero(500, currency.NewCurrency("USD", 10, 2)),
+			b:           dinero.NewDinero(500, currency.NewCurrency("USD", 10, 2)),
+			expect:      true,
+		},
+	}
+
+	for _, tc := range tests {
+		got := dinero.HaveSameCurrency(tc.a, tc.b)
+
+		if tc.expect != got {
+			t.Fatalf("%s expected a: %v, got: %v", tc.description, tc.expect, got)
+		}
+	}
+}
+
+func BenchmarkHaveSameCurrency(b *testing.B) {
+	da := dinero.NewDinero(100, currency.USD)
+	db := dinero.NewDinero(100, currency.USD)
+
+	for i := 0; i < b.N; i++ {
+		dinero.HaveSameCurrency(da, db)
+	}
+}
